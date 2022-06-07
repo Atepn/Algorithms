8c838bf4255c1d452550b7ff123e45bf12dd81a6
Atepn <lockerf@mail.ru>
2022-06-07 11:07:41 -0400

    First commit

--- square_sorting.py
+++ square_sorting.py
@@ -5,12 +5,3 @@ def insert_sort(list=[]):
             if list[j] < list[j-1]:
                 list[j], list[j-1] = list[j-1], list[j]
             else: break
-A=[1,4,5,2,5,6,7,2,4,3,6,5]
-insert_sort(A)
-print(A)
-A=[1,4,0,2,5,0,0,0,4,3,6,5]
-insert_sort(A)
-print(A)
-A=[1,4,5,2,-5,6,7,-2,-4,-3,6,-5]
-insert_sort(A)
-print(A)
