6b5a883314f8f54bd19a94efffb62655b42ec0c7
Atepn <lockerf@mail.ru>
2022-06-07 13:28:35 -0400

    First commit

--- square_sorting.py
+++ square_sorting.py
@@ -1,8 +1,29 @@
-def insert_sort(list=[]):
+def insert_sort(var_list=[]):
     """implements list sort by insert method"""
-    for i in range(1, len(list)):
+    for i in range(1, len(var_list)):
         for j in range(i, 0, -1):
-            if list[j] < list[j-1]:
-                list[j], list[j-1] = list[j-1], list[j]
+            if var_list[j] < var_list[j-1]:
+                var_list[j], var_list[j-1] = var_list[j-1], var_list[j]
             else: break
-print('hoba')
+def choice_sort(var_list=[]):
+    """implements list sort by choice method"""
+    for i in range(len(var_list)):
+        for j in range(i, len(var_list)):
+            if var_list[j] < var_list[i]:
+                var_list[j], var_list[i] = var_list[i], var_list[j]
+def bubble_sort(var_list=[]):
+    """implements list sort by bubble method"""
+    for i in range(len(var_list)):
+        for j in range(len(var_list)-i-1):
+            if var_list[j] > var_list[j+1]:
+                var_list[j], var_list[j+1] = var_list[j+1], var_list[j]
+
+A = [5,0,1,2,3,6,2,7,4,3]
+bubble_sort(A)
+print(A)
+A = [5,0,1,2,3,6,2,7,4,3]
+choice_sort(A)
+print(A)
+A = [5,0,1,2,3,6,2,7,4,3]
+insert_sort(A)
+print(A)
