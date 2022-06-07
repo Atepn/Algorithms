8bdf26a4ff558ba7728148e80eb01b1f31493991
Atepn <lockerf@mail.ru>
2022-06-07 09:50:05 -0400

    0.9

--- sieve_of_Eratosthenes.py
+++ sieve_of_Eratosthenes.py
@@ -1,1 +1,12 @@
-print('Hello')
+def is_prime_number(N):
+    """implements a prime number test by using sieve of Eratosthenes. returns a bool"""
+    if N < 2:
+        return False
+    nlist = [True]*(N+1)
+    # nlist[0] = nlist[1] = False
+    for i in range(2, N+1):
+        if nlist[i]:
+            for j in range(2*i, N+1, i):
+                nlist[j] = False
+    return nlist[N]
+print(is_prime_number(11))
